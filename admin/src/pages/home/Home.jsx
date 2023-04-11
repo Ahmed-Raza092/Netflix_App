7c0727e63aba9088cf2b76b3d292c01ffe91f067
Ahmed-Raza092 <you@example.com>
2023-04-11 16:53:22 +0500

    045304112023

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -4,15 +4,66 @@ import "./home.css";
 import { userData } from "../../dummyData";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
+import { useState } from "react";
+import { useEffect } from "react";
+
+
+import { useMemo } from "react";
+import axios from "axios"
 
 export default function Home() {
+  const MONTHS = useMemo(
+    () => [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    []
+  );
+
+  const [userStats, setUserStats] = useState([]);
+
+  useEffect(() => {
+    const getState = async () => {
+      try {
+        const res = await axios.get("/users/stats", {
+          headers: {
+            token:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MmVkNjk0OTkzZTZlOWU2NmIzNjg5YyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4MDk0Nzg1NCwiZXhwIjoxNjgxMzc5ODU0fQ.N0TCWKZQ7XqMKsVM8gUX1eoJyCDUSDlprDgt028Zwcw",
+          },
+        });
+        const statsList = res.data.sort(function (a, b) {
+          return a._id - b._id;
+        });
+        statsList.map((item) =>
+          setUserStats((prev) => [
+            ...prev,
+            { name: MONTHS[item._id - 1], "New User": item.total },
+          ])
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getState();
+  }, [MONTHS]);
+
   return (
     <div className="home">
       <FeaturedInfo />
-      <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+      <Chart data={userStats} title="User Analytics" grid dataKey="New User" />
       <div className="homeWidgets">
-        <WidgetSm/>
-        <WidgetLg/>
+        <WidgetSm />
+        <WidgetLg />
       </div>
     </div>
   );
